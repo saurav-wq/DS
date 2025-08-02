a91a38d28540f633bb4203e9cd486bcbbee44df3
SAURAV SUBEDI <subedisaurab73@gmail.com>
2025-08-02 22:59:30 +0545

    Add lab1 programs

--- lab1/q1.cpp
+++ lab1/q1.cpp
@@ -4,93 +4,38 @@ its output*/
 
 #include <iostream>
 using namespace std;
-
 int main() {
-    int A[10] = {0}, B[10] = {0}, n1, n2;
-    cout << "Enter the number of elements of A (max 10): ";
-    cin >> n1;
-    while (n1 < 1 || n1 > 10) {
-        cout << "Invalid input. Enter the number of elements of A (1-10): ";
-        cin >> n1;
-    }
-    cout << "Enter the elements of A: ";
-    for (int i = 0; i < n1; i++) {
-        cin >> A[i];
-    }
-    cout << "Enter the number of elements of B (max 10): ";
-    cin >> n2;
-    while (n2 < 1 || n2 > 10) {
-        cout << "Invalid input. Enter the number of elements of B (1-10): ";
-        cin >> n2;
-    }
-    cout << "Enter the elements of B: ";
-    for (int i = 0; i < n2; i++) {
-        cin >> B[i];
-    }
-
-    // Union
-    cout << "\nUnion: ";
+    int A[100], B[100], n1, n2;
+    cout << "Enter number of elements of A: " << endl; cin >> n1;
+    cout << "Enter elements of A: " << endl; for (int i = 0; i < n1; i++) cin >> A[i];
+    cout << "Enter number of elements of B: " << endl; cin >> n2;
+    cout << "Enter elements of B: " << endl; for (int i = 0; i < n2; i++) cin >> B[i];
+    cout << "\nUnion:" << endl;
     for (int i = 0; i < n1; i++) cout << A[i] << " ";
     for (int i = 0; i < n2; i++) {
-        bool found = false;
-        for (int j = 0; j < n1; j++) {
-            if (B[i] == A[j]) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) cout << B[i] << " ";
+        int cnt = 0;
+        for (int j = 0; j < n1; j++) if (B[i] == A[j]) cnt++;
+        if (cnt == 0) cout << B[i] << " ";
     }
-
-    // Intersection
-    cout << "\nIntersection: ";
+    cout << "\nIntersection:" << endl;
+    for (int i = 0; i < n1; i++) for (int j = 0; j < n2; j++) if (A[i] == B[j]) { cout << A[i] << " "; break; }
+    cout << "\nDifference (A - B):" << endl;
     for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n2; j++) {
-            if (A[i] == B[j]) {
-                cout << A[i] << " ";
-                break;
-            }
-        }
+        int cnt = 0;
+        for (int j = 0; j < n2; j++) if (A[i] == B[j]) cnt++;
+        if (cnt == 0) cout << A[i] << " ";
     }
-
-    // Difference A - B
-    cout << "\nDifference (A - B): ";
+    cout << "\nSymmetric Difference:" << endl;
     for (int i = 0; i < n1; i++) {
-        bool found = false;
-        for (int j = 0; j < n2; j++) {
-            if (A[i] == B[j]) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) cout << A[i] << " ";
+        int cnt = 0;
+        for (int j = 0; j < n2; j++) if (A[i] == B[j]) cnt++;
+        if (cnt == 0) cout << A[i] << " ";
     }
-
-    // Symmetric Difference
-    cout << "\nSymmetric Difference: ";
-    // A - B
-    for (int i = 0; i < n1; i++) {
-        bool found = false;
-        for (int j = 0; j < n2; j++) {
-            if (A[i] == B[j]) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) cout << A[i] << " ";
-    }
-    // B - A
     for (int i = 0; i < n2; i++) {
-        bool found = false;
-        for (int j = 0; j < n1; j++) {
-            if (B[i] == A[j]) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) cout << B[i] << " ";
+        int cnt = 0;
+        for (int j = 0; j < n1; j++) if (B[i] == A[j]) cnt++;
+        if (cnt == 0) cout << B[i] << " ";
     }
-
     cout << endl;
     return 0;
 }
--- lab1/q2.cpp
+++ lab1/q2.cpp
@@ -1,30 +1,34 @@
 
 /*2. Write a program that takes two or more sets as input and produces their
 Cartesian product as output.*/
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	int n,i;
-	int A[10];
-	int B[10];
-	cout<<"Enter the number of elements of set A: ";
-	cin>>n;
-	cout<<"Enter the elements: ";
-	for(i=0; i<n; i++) {
-		cin>>A[i];
-	}
-	cout<<"Enter the number of elements of set B: ";
-	cin>>n;
-	cout<<"Enter the elements: ";
-	for(i=0; i<n; i++) {
-		cin>>B[i];
-	}
-	cout<<"The cartesian product of (A x B):"<<endl;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			cout<<"("<<A[i]<<","<<B[j]<<")";
-		}
-	}
-return 0;
+int main() {
+    int n1, n2;
+    int A[10], B[10];
+
+    cout << "Enter the number of elements of set A: ";
+    cin >> n1;
+    cout << "Enter the elements of set A: ";
+    for (int i = 0; i < n1; i++) {
+        cin >> A[i];
+    }
+
+    cout << "Enter the number of elements of set B: ";
+    cin >> n2;
+    cout << "Enter the elements of set B: ";
+    for (int i = 0; i < n2; i++) {
+        cin >> B[i];
+    }
+
+    cout << "The Cartesian product of (A x B):" << endl;
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            cout << "(" << A[i] << "," << B[j] << ") ";
+        }
+    }
+
+    cout << endl;
+    return 0;
 }
